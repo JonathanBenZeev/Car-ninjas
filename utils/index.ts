2fd2e38f495a2abbaebea7b7843c9a2e9e72b299
Yonatan Ben Zeev <yonatanbz6@gmail.com>
2023-10-19 19:00:52 +0300

    adding card details

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types"
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': 'c0a2fb773cmshbda4b04a5dc66a0p12433fjsndbdc16cd505c',
@@ -28,3 +30,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0)
 }
+
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+} 
